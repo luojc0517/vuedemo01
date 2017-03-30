7b5557937e9ee184d7ac9160b305349e0f16298b
luojc0517 <luojiachen1@sina.com>
2017-03-30 23:23:07 +0800

    first commit

---
+++ src/components/List.vue
@@ -0,0 +1,36 @@
+<template>
+  <ol>
+    <li v-for="news in list">
+      <p class="title">{{news.title}}</p>
+      <p class="data">{{news.create_at}}</p>
+      <p class="author">{{news.author.loginname}}</p>
+    </li>
+  </ol>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+  ol {
+    margin-left: 2rem;
+    list-style: outside decimal;
+  }
+
+  li {
+    line-height: 1.5;
+    padding: 1rem;
+    border-bottom: 1px solid #b6b6b6;
+  }
+
+  .title {
+    font-weight: bold;
+    font-size: 1.3rem;
+  }
+
+  .date {
+    font-size: .8rem;
+    color: #d6d6d6;
+  }
+</style>
---
+++ src/components/LoadMore.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <list></list>
+    <a class="button" v-on:click="next">GO NEXT</a>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+  .button {
+    display: block;
+    width: 100%;
+    background: #212121;
+    color: #fff;
+    font-weight: bold;
+    text-align: center;
+    padding: 1em;
+    cursor: pointer;
+    text-decoration: none;
+  }
+
+  .button span {
+    margin-left: 2em;
+    font-size: .5rem;
+    color: #d6d6d6;
+  }
+</style>
